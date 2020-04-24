f20f466ef7a9e821b5ee643f6411e385b5d7b6ef
billmoriarty <wrmoriarty@gmail.com>
2020-04-24 14:55:26 -0400

    adding code and entity
    adding code and entity

---
+++ Bookworm/AddBookView.swift
@@ -0,0 +1,75 @@
+//
+//  AddBookView.swift
+//  Bookworm
+//
+//  Created by Bill Moriarty on 4/24/20.
+//  Copyright © 2020 Bill Moriarty. All rights reserved.
+//
+
+import SwiftUI
+
+struct AddBookView: View {
+    @Environment(\.managedObjectContext) var moc
+    @Environment(\.presentationMode) var presentationMode
+
+    
+    /*As this form is going to store all the data required to make up a book, we need @State properties for each of the book’s values except id, which we can generate dynamically */
+    @State private var title = ""
+    @State private var author = ""
+    @State private var rating = 3
+    @State private var genre = ""
+    @State private var review = ""
+    
+    let genres = ["Fantasy", "Horror", "Kids", "Mystery", "Poetry", "Romance", "Thriller"]
+
+    
+    var body: some View {
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Name of book", text: $title)
+                    TextField("Author's name", text: $author)
+
+                    Picker("Genre", selection: $genre) {
+                        ForEach(genres, id: \.self) {
+                            Text($0)
+                        }
+                    }
+                }
+
+                Section {
+                    Picker("Rating", selection: $rating) {
+                        ForEach(0..<6) {
+                            Text("\($0)")
+                        }
+                    }
+
+                    TextField("Write a review", text: $review)
+                }//section
+
+                Section {
+                    Button("Save") {
+                        // add the book
+                        let newBook = Book(context: self.moc)
+                        newBook.title = self.title
+                        newBook.author = self.author
+                        newBook.rating = Int16(self.rating)
+                        newBook.genre = self.genre
+                        newBook.review = self.review
+
+                        try? self.moc.save()
+                        self.presentationMode.wrappedValue.dismiss()
+                    }//button
+                    
+                }//section
+            }
+            .navigationBarTitle("Add Book")
+        }//NavigationView
+    }//body
+}//struct AddBookView
+
+struct AddBookView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddBookView()
+    }
+}
--- Bookworm/ContentView.swift
+++ Bookworm/ContentView.swift
@@ -9,13 +9,32 @@
 import SwiftUI
 
 struct ContentView: View {
+    //a way to add books
+    @Environment(\.managedObjectContext) var moc
+    //read all the books we have
+    @FetchRequest(entity: Book.entity(), sortDescriptors: []) var books: FetchedResults<Book>
+    
+    @State private var showingAddScreen = false
+    
     var body: some View {
-        Text("Hello, World!")
-    }
-}
+         NavigationView {
+            Text("Count: \(books.count)")
+                .navigationBarTitle("Bookworm")
+                .navigationBarItems(trailing: Button(action: {
+                    self.showingAddScreen.toggle()
+                }) {
+                    Image(systemName: "plus")
+                })//button
+                .sheet(isPresented: $showingAddScreen) {
+                                //...environment() takes two parameters: a key to write to, and the value you want to send in.
+                    AddBookView().environment(\.managedObjectContext, self.moc)
+                }
+        }// NavigationView
+    }//body
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
 }
